73458e1f11b74d1a281b80f57b087a7987988947
kislay-gupta <kislaygupta007@gmail.com>
2024-02-24 22:50:52 +0530

    feat: add card

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat, Raleway } from "next/font/google";
 import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const raleway = Raleway({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -17,10 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${montserrat.className} ${raleway.className}`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${montserrat.className} ${raleway.className}`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
