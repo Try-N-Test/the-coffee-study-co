a0bc72893125accbc5847688f90dfc74946bbb10
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-24 21:37:15 +0530

    fix: home page edits

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import PublicNavBar from "@/components/shared/NavBar";
 import Footer from "@/components/shared/Footer";
+import Hero from "@/components/home/Hero";
+import About from "@/components/home/About";
+import CTA from "@/components/home/CTA";
 
 export default function Home() {
   return (
     <>
       <PublicNavBar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-
+      <main className="flex min-h-screen flex-col">
+        <Hero />
+        <About />
+        <CTA />
+      </main>
       <Footer />
     </>
   );
