58fe1e160d675f56abdfafadde93f71cc88c3b8f
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-25 21:18:19 +0530

    fix: sidebar

--- src/app/(root)/communities/[id]/articles/page.tsx
+++ src/app/(root)/communities/[id]/articles/page.tsx
@@ -2,8 +2,9 @@ import ArticlesCard from "@/components/cards/ArticlesCards";
 import CommunityNavbar from "@/components/shared/CommunityNavbar";
 import SideBar from "@/components/shared/Sidebar";
 import React from "react";
+
 interface UrlProps {
-  params: string;
+  params: { id: string };
 }
 const Page = ({ params }: UrlProps) => {
   return (
@@ -13,7 +14,7 @@ const Page = ({ params }: UrlProps) => {
           <div style={{ width: "100%", height: "100vh", overflow: "scroll" }}>
             <CommunityNavbar />
             <div className="flex bg-[#F4F4F5]" style={{ width: "100%" }}>
-              <SideBar />
+              <SideBar dUrl={params.id}  />
               <div className=" pt-6" style={{ width: "100%" }}>
                 <ArticlesCard />
               </div>
