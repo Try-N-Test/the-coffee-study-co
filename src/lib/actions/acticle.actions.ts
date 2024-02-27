4bb767fae9743519404b3c639215d263306dc1d3
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-02-28 01:39:24 +0530

    feat:Added post server action

---
+++ src/lib/actions/acticle.actions.ts
@@ -0,0 +1,53 @@
+import { Post } from "../models/post.model";
+import { connectToDB } from "../mongoose";
+import Community from "../models/community.model";
+import { User } from "../models/user.model";
+import { revalidatePath } from "next/cache";
+
+export async function allPost(communityId: string) {
+  try {
+    connectToDB();
+    const posts = await Post.find({ communityId });
+    if (!posts) {
+      throw new Error("No posts found!");
+    }
+    return posts;
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+}
+
+interface AddPost {
+  communityId: string;
+  author: string;
+  title: string;
+  content: string;
+  pathname: string;
+}
+
+export async function addPost({
+  communityId,
+  author,
+  title,
+  content,
+  pathname,
+}: AddPost) {
+  try {
+    connectToDB();
+
+    const post = await Post.create({ communityId, author, title, content });
+    await post.save();
+
+    const community = await Community.findById(communityId);
+    community.posts.push(post._id);
+    await community.save();
+
+    const user = await User.findById(author);
+    user.posts.push(post._id);
+    await user.save();
+
+    revalidatePath(pathname);
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+}
